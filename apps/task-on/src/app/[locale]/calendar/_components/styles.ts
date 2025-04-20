8983d7ce5bc33f62753c28a9b1317e73b26ba8b5
awjae <djwotmd@gmail.com>
2025-04-20 23:23:16 +0900

    chore: 변수 모듈화

---
+++ apps/task-on/src/app/[locale]/calendar/_components/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material';
+
+export const CalendarWrapper = styled('div')`
+  width: 100%;
+  overflow-x: auto;
+  margin-top: 16;
+
+  .rbc-toolbar {
+    flex-direction: column;
+    gap: 12px;
+    margin-bottom: 16px;
+  }
+`;
